3f3b09b351919e5433603e9060ef805bf131319d
rigole <foplacide@gmail.com>
2022-04-17 11:07:06 +0100

    footer styles

--- food-web-app/src/components/Footer.js
+++ food-web-app/src/components/Footer.js
@@ -3,6 +3,7 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../styles/Footer.css";
 
 function Footer() {
 
@@ -15,6 +16,7 @@ function Footer() {
                 <LinkedInIcon/>
 
             </div>
+            <p>&copy; https://github.com/rigole/ReactJS-Applications 2022</p>
         </div>
     )
 }
