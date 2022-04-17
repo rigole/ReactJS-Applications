36f020f4318a39c646a23aa7e51c94f452d1c60a
rigole <foplacide@gmail.com>
2022-04-17 08:56:22 +0100

    fixing the home page + styling of the home page 1

--- food-web-app/src/App.js
+++ food-web-app/src/App.js
@@ -1,13 +1,16 @@
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import './App.css';
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
       <BrowserRouter>
           <Navbar/>
+          <Routes>
+              <Route  path="/" exact element={<Home/>} />
+          </Routes>
       </BrowserRouter>
     </div>
   );
--- food-web-app/src/pages/Home.js
+++ food-web-app/src/pages/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
-import banner from "../assets/photo.jpeg"
+import banner from "../assets/photo.jpeg";
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
 
     return(
-        <div className="home">
+        <div className="home" style={{ backgroundImage: `url(${banner})` }}>
             <div className="headerContainer">
+                <h1>Food Corner</h1>
+                <p>Foot from Africa</p>
+
+                    <button>Order Now</button>
 
             </div>
         </div>
