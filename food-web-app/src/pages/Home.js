3d31e436b0bd17bbd31493fc7d7b92369644d983
rigole <foplacide@gmail.com>
2022-04-17 10:37:34 +0100

    styling of the home page 2 + new banner

--- food-web-app/src/pages/Home.js
+++ food-web-app/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import banner from "../assets/photo.jpeg";
+import banner from "../assets/food.jpeg";
 import "../styles/Home.css";
 import { Link } from "react-router-dom";
 
