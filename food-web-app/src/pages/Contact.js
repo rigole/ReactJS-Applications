2fdd22b2521fc27223bc4ac5919d30c9984f8d20
rigole <foplacide@gmail.com>
2022-04-19 14:17:00 +0100

    contact page and its style 1

---
+++ food-web-app/src/pages/Contact.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Contact(){
+    return (
+        <div className="contact">
+            <div className="leftSide">
+               leftSide
+            </div>
+            <div className="rightSide">
+                <h1>Contact Us</h1>
+                <form id="contact-form" method="POST" action="">
+                    <label htmlFor="name">Full Name</label>
+                    <input name="name" type="text" placeholder="Enter your Name" />
+                    <label htmlFor="email">Email</label>
+                    <input name="name" placeholder="Enter email address" type="email"/>
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" placeholder="Enter your message" rows="7"></textarea>
+                    <button type="submit">Send Message</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+export default Contact
