8abc61ba0ce61a1c97421ee6ed8c78f2b3d1ccee
rigole <foplacide@gmail.com>
2022-04-20 13:13:16 +0100

    adding speed, pressure, humidity

--- weather-app/src/components/WeatherDetails.js
+++ weather-app/src/components/WeatherDetails.js
@@ -17,6 +17,47 @@ function WeatherDetails(){
                     </div>
                 </div>
                 <div className="date">{new Date().toLocaleString()}</div>
+                <div className="extra-temp">
+                    <div className="temp-info-minmax">
+                       <div className="two-sided-section">
+                            <p>
+                                <i className={"wi wi-sunset"}></i>
+                            </p>
+                            <p className="extra-info-leftside">
+                                10:30 PM <br/>
+                                Sunset
+                            </p>
+                       </div>
+                        <div className="two-sided-section">
+                            <p>
+                                <i className={"wi wi-humidity"}></i>
+                            </p>
+                            <p className="extra-info-leftside">
+                                444  <br/>
+                                Humidity
+                            </p>
+                        </div>
+                        <div className="two-sided-section">
+                            <p>
+                                <i className={"wi wi-rain"}></i>
+                            </p>
+                            <p className="extra-info-leftside">
+                                444  <br/>
+                                Pressure
+                            </p>
+                        </div>
+
+                        <div className="two-sided-section">
+                            <p>
+                                <i className={"wi wi-strong-wind"}></i>
+                            </p>
+                            <p className="extra-info-leftside">
+                                422  <br/>
+                                Speed
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </article>
         </div>
     )
