f30b7cf46bb91d06e8b18146a4814450572e2d40
rigole <foplacide@gmail.com>
2022-04-20 11:50:10 +0100

    search component

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import SearchMain from "./components/SearchMain";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <SearchMain/>
+    </div>
   );
 }
 
--- weather-app/src/components/SearchMain.js
+++ weather-app/src/components/SearchMain.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './style.css';
 
 function SearchMain(){
-    return (
-        <div>
+    const [longitude, setlongitude] = useState('9.7679')
+    const [latitude, setlatitude] = useState('4.0511');
 
+    return (
+        <div className="wrap">
+            <div className="search">
+                <input type="text" placeholder="enter Latitude" onChange={(e) => setlongitude(e.target.value)}/>
+                <input type="text" placeholder="enter longitude" onChange={(e) => setlatitude(e.target.value)}/>
+            </div>
+            <button className="searchButton">Search</button>
         </div>
     )
 }
