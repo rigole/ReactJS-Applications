080017a06f8e32a31e90e76802042a128e6c29b1
rigole <foplacide@gmail.com>
2022-04-20 12:18:10 +0100

    creating the weather components and rendering it in the Search page

--- weather-app/src/components/SearchMain.js
+++ weather-app/src/components/SearchMain.js
@@ -1,18 +1,32 @@
 import React, {useState} from 'react';
 import './style.css';
+import WeatherDetails from "./WeatherDetails";
 
 function SearchMain(){
-    const [longitude, setlongitude] = useState('9.7679')
+    const [longitude, setlongitude] = useState('9.7679');
     const [latitude, setlatitude] = useState('4.0511');
+    console.log(longitude);
+    console.log(latitude);
 
     return (
-        <div className="wrap">
-            <div className="search">
-                <input type="text" placeholder="enter Latitude" onChange={(e) => setlongitude(e.target.value)}/>
-                <input type="text" placeholder="enter longitude" onChange={(e) => setlatitude(e.target.value)}/>
+        <>
+            <div className="wrap">
+                <div className="search">
+                    <input type="text"
+                           placeholder="enter Latitude"
+                           value={longitude}
+                           onChange={(e) => setlongitude(e.target.value)}/>
+
+                    <input
+                        type="text"
+                        placeholder="enter longitude"
+                        value={latitude}
+                        onChange={(e) => setlatitude(e.target.value)}/>
+                </div>
+                <button className="searchButton">Search</button>
             </div>
-            <button className="searchButton">Search</button>
-        </div>
+            <WeatherDetails/>
+        </>
     )
 }
 
---
+++ weather-app/src/components/WeatherDetails.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function WeatherDetails(){
+    return(
+        <div>
+            <article className="widget">
+                <div className="weatherIcon">
+                    <i className="wi wi-night-sleet"></i>
+                </div>
+            </article>
+        </div>
+    )
+}
+
+export default WeatherDetails
