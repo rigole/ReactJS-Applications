ef9296067a36e86835c7d97f70a2d673309dba9b
rigole <foplacide@gmail.com>
2022-04-20 12:35:47 +0100

    weather component rendering 2

--- weather-app/src/components/WeatherDetails.js
+++ weather-app/src/components/WeatherDetails.js
@@ -5,7 +5,16 @@ function WeatherDetails(){
         <div>
             <article className="widget">
                 <div className="weatherIcon">
-                    <i className="wi wi-night-sleet"></i>
+                    <i className="wi wi-day-sunny"></i>
+                </div>
+                <div className="weatherInfo">
+                    <div className="temperature">
+                        <span>23.99&deg;</span>
+                    </div>
+                    <div className="description">
+                        <div className="weatherCondition">Sunny</div>
+                        <div className="place">Douala, CM</div>
+                    </div>
                 </div>
             </article>
         </div>
