a278742e3c74265599b59b58b0a23f75c849ac67
rigole <foplacide@gmail.com>
2022-04-24 11:00:28 +0100

    creating Header, Balance and IncomeExpense component

---
+++ expense-tracker/src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Balance = () => {
+    return (
+        <div>
+            <h4>Balance</h4>
+            <h1>35 000 FCFA</h1>
+        </div>
+    )
+}
---
+++ expense-tracker/src/components/IncomeExpense.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const IncomeExpense = () => {
+    return (
+        <div className="inc-exp-container">
+            <div>
+                <h4>Income</h4>
+                <p> 5000 FCFA </p>
+            </div>
+            <div>
+                <h4>Expenses</h4>
+                <p> 5000 FCFA </p>
+            </div>
+        </div>
+    )
+}
