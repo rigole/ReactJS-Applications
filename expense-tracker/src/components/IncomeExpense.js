e6b00aecfc92708dee19ca6218ce1ec17df6a331
rigole <foplacide@gmail.com>
2022-04-28 08:20:49 +0100

    balance setting 3

--- expense-tracker/src/components/IncomeExpense.js
+++ expense-tracker/src/components/IncomeExpense.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {useContext} from "react";
+import { GlobalContext } from "../context/GlobalSate";
+
+
+function moneyFormatter(num){
+    let p = num.toFixed(2).split(".");
+    return (
+        p[0].split("").reverse().reduce(function (acc, num, i, orig){
+            return num === "-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
+        }, "" ) + "." + p[1] + "FCFA"
+    );
+}
 
 export const IncomeExpense = () => {
+
+    const {transactions} = useContext(GlobalContext);
+    const {amounts} = transactions.map((transaction) => transaction.amount);
+    const income = amounts.filter((item) => item > 0).reduce((acc, item) => (acc += item), 0);
+
+    const expense = amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) * -1
+
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p> 5000 FCFA </p>
+                <p className="money plus">{moneyFormatter(income)}</p>
             </div>
             <div>
-                <h4>Expenses</h4>
-                <p> 5000 FCFA </p>
+                <h4 >Expenses</h4>
+                <p className="money minus"> {moneyFormatter(expense)} </p>
             </div>
         </div>
     )
