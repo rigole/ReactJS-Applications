9ef7f90489b0cecf6c831c91e3bc424a8f50e75a
rigole <foplacide@gmail.com>
2022-04-30 13:56:20 +0100

    creating function for pomorodo

--- pomorodo-app/src/components/Pomorodo.js
+++ pomorodo-app/src/components/Pomorodo.js
@@ -1,6 +1,44 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 
 function Pomorodo() {
+
+    const [newTimer, setNewTimer] = useState({
+        work: 0.2,
+        short: 0.1,
+        long: 0.5,
+        active: 'work'
+    })
+
+    const {updateExecute} = useContext(SettingsContext)
+    const handleChange = input => {
+        const {name, value} = input.target
+        switch (name) {
+            case "work" :
+                setNewTimer({
+                    ...newTimer,
+                    work: parseInt(value),
+                })
+                break;
+            case "shortBreak":
+                setNewTimer({
+                    ...newTimer,
+                    short: parseInt(value),
+                })
+                break;
+            case "longBreak":
+                setNewTimer({
+                    ...newTimer,
+                    long: parseInt(value)
+                })
+                break;
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        updateExecute(newTimer)
+    }
+
     return (
         <div className="form-container">
             <form>
