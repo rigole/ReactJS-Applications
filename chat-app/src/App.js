9dedc66b72f802a5e498ffdb84919be987ed6ac8
rigole <foplacide@gmail.com>
2022-05-06 12:42:11 +0100

    testing the main interface

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -5,6 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <ChatEngine
+          height="100vh"
+          projectID="e50f29fc-c630-4eab-bee2-f1f9097c5aaf"
+          userName="rigolux"
+          userSecret="123456"
+      />
     </div>
   );
 }
