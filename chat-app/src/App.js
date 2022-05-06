6b39fb5c3e6402835803ddf6f9c13ad3df648cb8
rigole <foplacide@gmail.com>
2022-05-06 13:39:34 +0100

    creating the needed components

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,4 +1,5 @@
-import { ChatEngine } from "react-chat-engine";
+import {ChatEngine } from "react-chat-engine";
+import ChatFeed from  './components/ChatFeed.jsx'
 import './App.css';
 
 
@@ -10,6 +11,8 @@ function App() {
           projectID="e50f29fc-c630-4eab-bee2-f1f9097c5aaf"
           userName="rigolux"
           userSecret="123456"
+          renderChatFeed = { (chatAppProps) => <ChatFeed {...chatAppProps}/>}
+          onNewMessage = { () => Audio('https://chat-engine-assets-s3.amazonaws/click.mp3').play()}
       />
     </div>
   );
