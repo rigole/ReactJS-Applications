626fe8f32271ee0c0e27514af5b576f251ac76c0
rigole <foplacide@gmail.com>
2022-05-10 09:42:32 +0100

    creating the LoginForm component 2

--- chat-app/src/components/LoginForm.jsx
+++ chat-app/src/components/LoginForm.jsx
@@ -4,9 +4,46 @@ import axios from "axios";
 const projectID="e50f29fc-c630-4eab-bee2-f1f9097c5aaf";
 
 const LoginForm = () => {
-    return (
-     <div>
 
-     </div>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const[error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+      //  const authObject = {'Project-ID': projectID, 'User-Name': username, 'User-Secret'}
+    }
+
+    return (
+         <div className="wrapper">
+            <div className="form">
+               <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={}
+                        onChange={ (e) => setUsername(e.target.value)}
+                        className="input"
+                        placeholder="Username"
+                        required
+                    />
+                    <input
+                        type="password"
+                        value={}
+                        onChange={ (e) => setPassword(e.target.value)}
+                        className="input"
+                        placeholder="Password"
+                        required
+                    />
+                    <div align="center">
+                        <button className="button">
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                </form>
+                <h1>{error}</h1>
+            </div>
+         </div>
     )
 }
