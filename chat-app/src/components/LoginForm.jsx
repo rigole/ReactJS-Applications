648ef2d7823414809b35c0efb99e48c1438c9b92
rigole <foplacide@gmail.com>
2022-05-10 10:07:52 +0100

    creating the handleSubmit function for login

--- chat-app/src/components/LoginForm.jsx
+++ chat-app/src/components/LoginForm.jsx
@@ -12,7 +12,19 @@ const LoginForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-      //  const authObject = {'Project-ID': projectID, 'User-Name': username, 'User-Secret'}
+        const authObject = {'Project-ID': projectID, 'User-Name': username, 'User-Secret': password};
+
+        try {
+            await axios.get('https://api.chatengine.io/chats', {headers: authObject})
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+            setError('');
+
+        }catch (err) {
+            setError('Oops Incorrect credentials')
+        }
     }
 
     return (
