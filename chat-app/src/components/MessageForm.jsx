40f351d0c27c7f0b61b6b31499ad9fe0119ebdeb
rigole <foplacide@gmail.com>
2022-05-10 08:30:56 +0100

    setting MessageForm component 1

--- chat-app/src/components/MessageForm.jsx
+++ chat-app/src/components/MessageForm.jsx
@@ -1,8 +1,29 @@
-const MessageForm = () => {
+import { useState } from "react";
+import { SendOutlined, PictureOutlined } from "@ant-design/icons";
+
+import { sendMessage, isTyping } from "react-chat-engine";
+
+const MessageForm = (props) => {
+
+    const [value, setValue] = useState('');
+    const { chatId, creds } = props;
+
     return (
-        <div>
-            My Message
-        </div>
+        <form>
+           <input
+                className="message-input"
+                placeholder="Send a message..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+           />
+
+            <label htmlFor="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon"/>
+                </span>
+            </label>
+        </form>
     )
 }
 
