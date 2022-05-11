089f94ede39f0881a4e0dd177026692b7ae32a1f
rigole <foplacide@gmail.com>
2022-05-11 15:21:27 +0100

    creating and setting the Link component

---
+++ search-engine-app/src/components/Links.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const links = [
+    {url: './search', text: "🔍 All"},
+    {url: './news', text: '📰 News'},
+    {url: './images', text: '📷 Images'},
+    {url: './videos', text: '🎥 Videos'}
+]
+
+function Links() {
+    return (
+        <div className="flex sm:justify-around justify-between items-center mt-4">
+            {links.map(({url, text}) => (
+                <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2"/>
+            ))}
+        </div>
+    )
+}
+export default Links
