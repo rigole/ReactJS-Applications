3d8de1da7f1a6b6ae2cd44443eda652c13af4c79
rigole <foplacide@gmail.com>
2022-05-11 13:58:36 +0100

    component search and debounce and use-debounce

--- search-engine-app/src/index.js
+++ search-engine-app/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <Router>
+         <App/>
+    </Router>
 );
 
