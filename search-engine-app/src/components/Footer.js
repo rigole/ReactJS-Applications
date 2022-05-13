44c88b0b190654b0da32d67596dfbd940bb8578d
rigole <foplacide@gmail.com>
2022-05-13 11:37:59 +0100

    Link in search and Loading + Footer components

---
+++ search-engine-app/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+            &copy 2022 237-Search, Inc.
+        </div>
+    )
+}
+export default Footer
--- search-engine-app/src/components/Search.js
+++ search-engine-app/src/components/Search.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {useDebounce} from "use-debounce";
+import  Links  from './Links';
 
 function Search() {
     const[setSearchTerm] = useState()
@@ -17,7 +18,16 @@ function Search() {
         <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
             <input type="text" className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm
             outline-none p-6 text-black hover:shadow-lg" placeholder="Search Using 237Search 🔍" onChange={(e) => setText(e.target.value)}/>
-            {text !== "" && (<button type="button" className="absolute top-1.5 right-4 text-1xl text-gray-500" onClick={() => setText("")}>X</button>)}
+            {text !== "" &&
+                (<button
+                    type="button"
+                    className="absolute top-1.5 right-4 text-1xl text-gray-500"
+                    onClick={() => setText("")}
+                >
+                    X
+                </button>
+                )}
+            <Links/>
         </div>
     )
 }
