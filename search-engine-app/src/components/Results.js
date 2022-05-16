cafcc32c9d27b6b0a5cdad297bf4e839ae6e0423
rigole <foplacide@gmail.com>
2022-05-16 11:35:14 +0100

    setting the return block for the video case

--- search-engine-app/src/components/Results.js
+++ search-engine-app/src/components/Results.js
@@ -40,6 +40,77 @@ function Results(){
                 </div>
             );
 
+
+        case './images' :
+            return (
+                <div className="flex flex-wrap justify-center items-center">
+                    {results?.image_results?.map(
+                        ({ image, link: {href, title} }, index) => (
+                            <a
+                                href={href}
+                                target="_blank"
+                                key={index}
+                                rel="noreferrer"
+                                className="sm:p-3 p-5"
+                            >
+                                <img src={image?.src} alt={title} loading="lazy"/>
+                                <p className="sm:w-36 w-36 break-words text-sm mt-2">{title}</p>
+                            </a>
+                        )
+                    )}
+
+                </div>
+            );
+
+
+        case './news' :
+            return (
+                <div className="sm:px-56 flex flex-wrap justify-between ite,s-center space-y-6">
+                    {results?.entries?.map(({ id, links, source, title }) => (
+                        <div >
+                            <a
+                                href={links?.[0].href}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="hover:underline"
+                            >
+                                <p className="text-lg dark:text-blue-300 text-blue-700">
+                                    {title}
+                                </p>
+                            </a>
+                            <div className="flex gap-4">
+                                <a
+                                    href={source?.href}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="hover:underline hover:text-blue-300"
+                                >
+                                    {"  "}
+                                    {source?.href}
+                                </a>
+                             </div>
+                        </div>
+                    ))}
+                </div>
+            );
+
+        case './videos' :
+            return (
+                <div className="flex flex-wrap">
+                    {results?.results?.map((video, index) =>(
+                        <div key={index} className="p-2">
+                            <ReactPlayer
+                                url={video.additional_links?.[0].href}
+                                controls
+                                width="355px"
+                                height="200px"
+                            />
+                        </div>
+                    ))}
+                </div>
+            );
+        default:
+            return "Error...";
     }
 
 }
