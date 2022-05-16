951e3e4ce73596c951c681c235fbf8c201e746fb
rigole <foplacide@gmail.com>
2022-05-16 11:49:28 +0100

    creating the routes components

---
+++ search-engine-app/src/components/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Switch, Route, Redirect} from "react-router-dom";
+
+import {Results} from './Results'
+
+function Routes(){
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Routes
