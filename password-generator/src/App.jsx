bdde636e94b076990e8c0636aa14288bf23fafc6
rigole <foplacide@gmail.com>
2022-05-25 11:20:01 +0100

    setting the app

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -2,8 +2,59 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container">
+      <div className="generator">
+          <h2>Password Generator</h2>
+        <div className="generator_password">
+            <button>Clipboard</button>
+        </div>
+          <div className="form-group">
+              <label htmlFor="password-length">Password Length</label>
+              <input
+                  name="password-length"
+                  id="password-length" type="number"
+                  max="20"
+                  min="7"
+              />
+          </div>
+
+          <div className="form-group">
+              <label htmlFor="">Include Uppercase Letters</label>
+              <input
+                  id="uppercase-letters"
+                  name="uppercase-letters"
+                  type="checkbox"
+              />
+          </div>
+
+          <div className="form-group">
+              <label htmlFor="include lowercase-letters">Include Lower Case Letters</label>
+              <input
+                id="lowercase-letters"
+                className="lowercase-letters"
+                type="checkbox"
+              />
+          </div>
+
+          <div className="form-group">
+              <label htmlFor="include-numbers">Include numbers</label>
+              <input
+                  id="include-numbers"
+                  className="include-numbers"
+                  type="checkbox"
+              />
+          </div>
+
+
+          <div className="form-group">
+              <label htmlFor="inclued-symbols">Include Symbols</label>
+              <input
+                  id="inclued-symbols"
+                  className="inclued-symbols"
+                  type="checkbox"
+              />
+          </div>
+      </div>
     </div>
   );
 }
