eb28026c07479a5c893cc9d7915f63412a26d13d
rigole <foplacide@gmail.com>
2022-06-13 13:43:57 +0100

    Header component

---
+++ note-saver-app/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+
+    const logo = (
+        <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
+             alt="logo"
+        />
+    )
+
+    return (
+     <div>{logo} <h1>Saving</h1></div>
+    )
+
+}
+
+export default Header
