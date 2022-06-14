a72f7cee62611242a1bf04f0ec7a340752a21486
rigole <foplacide@gmail.com>
2022-06-14 11:58:05 +0100

    NoteArea  and Count component 2

--- note-saver-app/src/components/NoteArea.js
+++ note-saver-app/src/components/NoteArea.js
@@ -3,7 +3,11 @@ import React from "react"
 function NoteArea() {
     return (
         <div>
-
+            <form >
+                <input type="text" placeholder="Title" name="title"/>
+                <textarea name="content" placeholder="Write a note here"></textarea>
+                <button>+</button>
+            </form>
         </div>
     )
 }
