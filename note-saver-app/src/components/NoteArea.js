76da989e24938563bf0b6433ee362f936cb43930
rigole <foplacide@gmail.com>
2022-06-14 12:48:59 +0100

    adding material design package and icons component to noteArea

--- note-saver-app/src/components/NoteArea.js
+++ note-saver-app/src/components/NoteArea.js
@@ -1,12 +1,14 @@
 import React from "react"
-
+import AddIcon from '@mui/icons-material/Add';
 function NoteArea() {
     return (
         <div>
             <form >
                 <input type="text" placeholder="Title" name="title"/>
                 <textarea name="content" placeholder="Write a note here"></textarea>
-                <button>+</button>
+                <button>
+                    <AddIcon/>
+                </button>
             </form>
         </div>
     )
