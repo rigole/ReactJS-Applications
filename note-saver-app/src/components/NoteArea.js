6f5b7c5cfb0903aa1a85e74ad0d2e7fb3d82a799
rigole <foplacide@gmail.com>
2022-06-16 16:13:26 +0100

    HandleChange, SubmitButton function

--- note-saver-app/src/components/NoteArea.js
+++ note-saver-app/src/components/NoteArea.js
@@ -1,11 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 import AddIcon from '@mui/icons-material/Add';
 function NoteArea() {
+    const [expanded, setExpanded] = useState(false)
+    const [note, setNote] = useState({
+        title: "",
+        content: "",
+    })
+
+    function handleChange(e) {
+        const {name, value} = e.target;
+        setNote((preValue) => {
+            return {
+                ...preValue,
+                [name] : value,
+            }
+        })
+    }
+
+    function submitButton(e) {
+        onAdd(note);
+        setNote({
+            title:"",
+            content:"",
+        });
+
+        e.preventDefault()
+    }
+
     return (
         <div>
             <form >
                 <input type="text" placeholder="Title" name="title"/>
-                <textarea name="content" placeholder="Write a note here"></textarea>
+                <textarea
+                    name="content"
+                    placeholder="Write a note here"
+                />
                 <button>
                     <AddIcon/>
                 </button>
