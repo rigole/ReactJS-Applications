670f47e68d88a29f0322015e70d3cda66a525784
rigole <foplacide@gmail.com>
2022-07-03 19:50:22 +0100

    single component

--- color-generators/src/SingleColor.js
+++ color-generators/src/SingleColor.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import rgbToHex from './utils'
 
-function SingleColor(){
+function SingleColor({rgb, weight, index, hexColor}){
+    const [alert, setAlert] = useState(false)
+    const bcg = rgb.join(",")
+    const hex = rgbToHex(...rgb)
     return (
-        <div>
+            <article className={`color ${index > 10 && 'color-light'}`} style={{background: `rgb(${bcg})`}}>
+                <p className="percentage-value">{weight}</p>
+                <p className="color">{hexColor}</p>
+            </article>
 
-        </div>
     )
 }
 export default SingleColor
