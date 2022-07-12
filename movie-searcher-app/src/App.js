a3ec2783e8ae7f718fe187f65b80c7ee71beee42
rigole <foplacide@gmail.com>
2022-07-12 13:52:35 +0100

    rendering poster images and new component

--- movie-searcher-app/src/App.js
+++ movie-searcher-app/src/App.js
@@ -1,9 +1,46 @@
-
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MovieList from "./components/MovieList";
 
 function App() {
+  const [movies, setMovies] = useState([
+      {
+          "Title": "Star Wars",
+          "Year": "1977",
+          "imdbID": "tt0076759",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BNzg4MjQxNTQtZmI5My00YjMwLWJlMjUtMmJlY2U2ZWFlNzY1XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
+      },
+      {
+          "Title": "Star Wars: Episode V - The Empire Strikes Back",
+          "Year": "1980",
+          "imdbID": "tt0080684",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+      },
+      {
+          "Title": "Star Wars: Episode VI - Return of the Jedi",
+          "Year": "1983",
+          "imdbID": "tt0086190",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+      },
+      {
+          "Title": "Star Wars: Episode VII - The Force Awakens",
+          "Year": "2015",
+          "imdbID": "tt2488496",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
+      },
+  ])
   return (
-    <div className="App">
+    <div className="container-fluid movie-app">
+      <div className="row">
+          <MovieList
+            movies={movies}
+          />
+      </div>
     </div>
   );
 }
