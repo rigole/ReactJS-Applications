9d4b79dba293ba3c148be57c0214a26f136f2b7d
rigole <foplacide@gmail.com>
2022-07-13 15:57:23 +0100

    rendering movie through search

--- movie-searcher-app/src/App.js
+++ movie-searcher-app/src/App.js
@@ -1,24 +1,40 @@
 import React, { useState, useEffect } from 'react'
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import MovieList from "./components/MovieList";
 import MovieHeader from "./components/MovieHeader";
 import SearchBox from "./components/SearchBox";
 
 function App() {
-  const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([])
     const [searchValue, setSearchValue] = useState('')
-  return (
-    <div className="container-fluid movie-app">
-        <div className="row d-flex align-items-center mt-4 mb-4">
-            <MovieHeader heading="Movies"/>
+
+    const getMovieRequest = async (searchValue) => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=8aa0d340`
+
+        const  response = await fetch(url)
+        const responseJson = await response.json()
+
+        if (responseJson.Search){
+            setMovies(responseJson.Search)
+        }
+    }
+
+    useEffect(() => {
+        getMovieRequest(searchValue)
+    },[searchValue])
+      return (
+        <div className="container-fluid movie-app">
+            <div className="row d-flex align-items-center mt-4 mb-4">
+                <MovieHeader heading="Movies"/>
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+            </div>
+          <div className="row">
+              <MovieList movies={movies}/>
+
+          </div>
         </div>
-      <div className="row">
-          <MovieList movies={movies}/>
-          <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-      </div>
-    </div>
-  );
+      );
 }
 
 export default App;
